885067fc2914d91d5786440e46d65a5fc2e7bfc4
brunningman <abrunner221@gmail.com>
2019-07-08 22:30:52 -0700

    refactored out of react-express-views

---
+++ client/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Book from './Modules/book.jsx';
+import Author from './Modules/author.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+export default App;
--- server/index.js
+++ server/index.js
@@ -5,37 +5,21 @@ const axios = require('axios');
 
 const morgan = require('morgan');
 
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
+// app.set('view engine', 'jsx');
+// app.engine('jsx', require('express-react-views').createEngine());
 app.use(morgan('dev'));
 
-app.use(express.static(__dirname + '/../public'));
 
-app.get('/books', (req, res) => {
-  res.end();
+// app.get('/books', (req, res) => {
+//   res.end();
+// });
+
+app.get('/books/:id', (req, res, next) => {
+  console.log('RUNNING');
+  res.write(req.params.id);
+  next();
 });
 
-app.get('/books/:id', (req, res) => {
-  let bookId = req.params.id;
-  axios({
-    method: 'get',
-    url: `http://localhost:3030/book/${bookId}`
-  })
-    .then(response => {
-      return response.data[0];
-    })
-    .then(book => {
-      axios({
-        method: 'get',
-        url: `http://localhost:3002/authors/${book.author.id}`
-      })
-        .then(response => {
-          book.author = response.data;
-          res.render('index', { book: book });
-        })
-        .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
-});
+app.use('/books/:id', express.static(__dirname + '/../public'));
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
