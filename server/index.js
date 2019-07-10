1ace5c917ebd8b9312517273a5177a2a8c37b60b
brunningman <abrunner221@gmail.com>
2019-07-10 08:46:49 -0700

    latest commit

--- server/index.js
+++ server/index.js
@@ -1,24 +1,16 @@
 const express = require('express');
 const port = 3000;
 const app = express();
-const axios = require('axios');
 
 const morgan = require('morgan');
 
-// app.set('view engine', 'jsx');
-// app.engine('jsx', require('express-react-views').createEngine());
 app.use(morgan('dev'));
 
-
-// app.get('/books', (req, res) => {
-//   res.end();
+// app.get('/books/:id', (req, res, next) => {
+//   console.log('RUNNING');
+//   res.write(req.params.id);
+//   next();
 // });
-
-app.get('/books/:id', (req, res, next) => {
-  console.log('RUNNING');
-  res.write(req.params.id);
-  next();
-});
 
 app.use('/books/:id', express.static(__dirname + '/../public'));
 
